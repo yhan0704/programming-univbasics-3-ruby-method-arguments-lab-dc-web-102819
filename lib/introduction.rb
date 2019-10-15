48d977158d1a99a2c6a2d4b38f097a31f6da3d61
pleasant-controller-2669 <han20@umbc.edu>
2019-10-15 20:00:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,8 +5,8 @@
 
 
 
-def introduction(name)
-  puts "Hi, my name is #{name}."
+def introduction(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 
